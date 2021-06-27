03f2538b11aec08a3bf4a11004064bfa5c7157d0
Reemaalhumdan <86562000+Reemaalhumdan@users.noreply.github.com>
2021-06-27 19:12:45 +0300

    Add files via upload

---
+++ قاعدة بيانات واجهتين التحكم بالذراع.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 26 يونيو 2021 الساعة 15:30
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arm_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `control`
+--
+
+CREATE TABLE `control` (
+  `motor1` int(11) NOT NULL,
+  `motor2` int(11) NOT NULL,
+  `motor3` int(11) NOT NULL,
+  `motor4` int(11) NOT NULL,
+  `motor5` int(11) NOT NULL,
+  `motor6` int(11) NOT NULL,
+  `on` tinyint(1) NOT NULL,
+  `off` tinyint(1) NOT NULL,
+  `top` int(11) NOT NULL,
+  `bottom` int(11) NOT NULL,
+  `left` int(11) NOT NULL,
+  `rigth` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
